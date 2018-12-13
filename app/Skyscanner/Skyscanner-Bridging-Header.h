209d83d52e9878c37c7cf359f856f006f8577f8e
Ricardo Casanova <ricardo@pijp.eu>
2018-12-13 14:28:07 +0100

    Add Skyscanner-Bridging-Header file

---
+++ app/Skyscanner/Skyscanner-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Skyscanner-Bridging-Header.h
+//  Skyscanner
+//
+//  Created by Ricardo Casanova on 13/12/2018.
+//  Copyright © 2018 Skyscanner. All rights reserved.
+//
+
+#ifndef Skyscanner_Bridging_Header_h
+#define Skyscanner_Bridging_Header_h
+
+//__ Haneke
+#import "Haneke.h"
+
+//__ SVProgressHUD
+#import "SVProgressHUD.h"
+
+#endif /* Skyscanner_Bridging_Header_h */
